a8a633fce93b5ad8a8a84b83ab20215f41005cad
solbarre <45764699+solbarre@users.noreply.github.com>
2024-04-30 16:04:28 +0200

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,21 +1,21 @@
 const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-exports.handler = async function (event, context) {
-  try {
-	await ddb
-	  .put({
-		TableName: 'ma_table',
-		Item: {
-		  connectionId: event.requestContext.connectionId,
-		},
-	  })
-	  .promise();
-  } catch (err) {
-	return {
-	  statusCode: 500,
-	};
-  }
-  return {
-	statusCode: 200,
-  };
+
+// Create DynamoDB Document Client
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+    try {
+        const connectionId = event.requestContext.connectionId;
+        const params = {
+            TableName: 'ma_table', // Replace 'yourTableName' with your DynamoDB table name
+            Item: {
+                connectionId: connectionId
+            }
+        };
+        await dynamodb.put(params).promise();
+        return { statusCode: 200, body: 'Connected to DynamoDB' };
+    } catch (error) {
+        console.error('Error connecting to DynamoDB:', error);
+        return { statusCode: 500, body: 'Error connecting to DynamoDB' };
+    }
 };
